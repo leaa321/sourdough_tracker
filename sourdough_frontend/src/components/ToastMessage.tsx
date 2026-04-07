94dcc2f01bcf18c70fa560daf7e71affe59d1556
Lea Drewes <lea.drewes@hec.de>
2026-04-07 14:18:36 +0200

    feat styled toast

--- sourdough_frontend/src/components/ToastMessage.tsx
+++ sourdough_frontend/src/components/ToastMessage.tsx
@@ -1,23 +1,33 @@
 import { useState } from "react"
+import "../style/ToastMessage.scss"
 
 type ToastProps = {
     message: string;
     visible: boolean;
+    type: string;
 };
 
-export function ToastMessage({ message, visible }: ToastProps) {
+export function ToastMessage({ message, visible, type }: ToastProps) {
     if (!visible) return null;
 
-    return <div className="toast">{message}</div>;
+
+    return (
+        <>
+            {type === "success" && <div className="toast success">{message}</div>}
+            {type === "error" && <div className="toast error">{message}</div>}
+        </>
+    )
 }
 
 export function useToast() {
     const [visible, setVisible] = useState(false);
     const [message, setMessage] = useState("");
+    const [type, setType] = useState("success");
 
-    function showToast(newMessage: string, duration = 2000) {
+    function showToast(newMessage: string, duration = 2000, type: string) {
         setMessage(newMessage);
         setVisible(true);
+        setType(type);
 
         setTimeout(() => {
             setVisible(false);
@@ -27,6 +37,7 @@ export function useToast() {
     return {
         visible,
         message,
+        type,
         showToast
     };
 }
